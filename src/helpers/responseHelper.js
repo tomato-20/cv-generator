434544442738eed209cd0a0649ae5d9e28607a94
tomato-20 <programmer.anon.01@gmail.com>
2022-07-07 14:27:06 +0545

    add response helper

--- src/helpers/responseHelper.js
+++ src/helpers/responseHelper.js
@@ -1,5 +1,20 @@
 ((ResponseHelper) => { 
-    ResponseHelper.successResponse = () => {
-        
+
+    const {StatusCodes} = require('http-status-codes')
+
+    ResponseHelper.successResponse = (message, data, res) => {
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            data,
+            message
+        })
+    },
+
+
+    ResponseHelper.errorResponse = (status, message, res) => {
+        return res.status(status || StatusCodes.BAD_REQUEST).json({
+            success: false,
+            message: message || "Error message"
+        })
     }
 })(module.exports)
