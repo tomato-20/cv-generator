d63062329255ad113be8ea41ff8ee20ae8e08ff9
tomato-20 <programmer.anon.01@gmail.com>
2022-07-15 15:30:54 +0545

    edit token update when login

--- src/modules/Auth/user.login.js
+++ src/modules/Auth/user.login.js
@@ -17,20 +17,19 @@ exports.login = async (req, res, next) => {
     const oldToken = await req.db.collection("token").findOne({ userId : existedUser._id });
 
     //to create a token
-    const token = jwt.sign({ id: existedUser._id }, process.env.SECRET);
+    const token = jwt.sign({ id: existedUser._id }, process.env.TOKEN_SECRET);
 
     // save user in db
     if (oldToken) {
       let updatetedUser = await req.db
         .collection("token")
         .updateOne({ token: oldToken.token }, { $set: { token } });
+        return responseHelper.successResponse(res, "User logged in", { token });
     } else {
       await req.db.collection("token").insertOne({ userId : existedUser._id, token });
-
       return responseHelper.successResponse(res, "User logged in", { token });
     }
-    return responseHelper.errorResponse(res, "Login unsucessfull");
   } catch (error) {
-    throw error;
+    return responseHelper.errorResponse(res, error.message || "Login unsucessfull");
   }
 };
